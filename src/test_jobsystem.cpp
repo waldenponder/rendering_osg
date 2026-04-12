d27ff68b65aad58058ce95f564b5b040a9efa5b2
wangstu <wangqianstu@163.com>
2026-04-12 08:07:31 +0800

    -

--- src/test_jobsystem.cpp
+++ src/test_jobsystem.cpp
@@ -46,18 +46,46 @@ void test4()
     js.emancipate();
 }
 
+//void tt5() {
+//  utils::JobSystem *js_p = new utils::JobSystem;
+//  auto &js = *js_p;
+//
+//  js.adopt();
+//  auto *parent =
+//      utils::jobs::createJob(js, nullptr, [&js](JobSystem::Job *self) {
+//        // 只有 parent 跑起来以后，才产生 c1 和 c2
+//        auto *c1 = utils::jobs::createJob(js, self, [] { /* 子任务1 */ });
+//        auto *c2 = utils::jobs::createJob(js, self, [] { /* 子任务2 */ });
+//
+//        js.run(c1);
+//        js.run(c2);
+//
+//        // Parent 自身的逻辑...
+//        std::cout << "Parent working...\n";
+//      });
+//
+//  // 主线程只需要管好 parent 即可
+//  js.runAndWait(parent);
+//}
+
 void test_jobsystem()
 {
-    test4();
-    return;
+    // tt5();
+    //return;
     utils::JobSystem* js_p = new utils::JobSystem;
     auto& js = *js_p;
 
     js.adopt();
 
-    auto* parent = js.createJob();
+    auto* parent = utils::jobs::createJob(js, nullptr, []
+    {
+        //
+        std::cout << "\nchild p\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << "child p job done\n";
+    });
 
-    utils::jobs::createJob(js, parent, []
+    auto c1 = utils::jobs::createJob(js, parent, []
     {
         //
         std::cout << "child 1\n";
@@ -65,14 +93,16 @@ void test_jobsystem()
         std::cout << "child 1 job done\n";
     });
 
-    utils::jobs::createJob(js, parent, []
+    auto c2 = utils::jobs::createJob(js, parent, []
     {
         //
         std::cout << "child 2\n";
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         std::cout << "child 2 job done\n";
     });
-
+    js.run(c1);
+    js.run(c2);
+    //js.run(parent);
     js.runAndWait(parent);
     std::cout << "\n";
 }
