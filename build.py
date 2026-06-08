b44f611dcafbed219dcf2f429c1816a7a8cb5ac7
wangstu <wangqianstu@163.com>
2026-06-08 21:58:19 +0800

    add build.py

---
+++ build.py
@@ -0,0 +1,28 @@
+import subprocess
+import os
+import shutil
+
+print(shutil.which("cmake"))
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+source_dir = script_directory  # CMakeLists.txt所在目录
+build_dir = os.path.join(script_directory,  "build")
+
+os.makedirs(build_dir, exist_ok=True)
+
+# 配置
+subprocess.check_call([
+    "cmake",
+    "-S", source_dir,
+    "-B", build_dir
+])
+
+# 编译
+subprocess.check_call([
+    "cmake",
+    "--build", build_dir,
+    "--config", "Debug"
+])
+
+print("Build Success")
